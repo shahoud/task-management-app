9202f55b5098166c9dae6658cd6e15dc23b3b31f
Shahoud <eng.shahoud@gmail.com>
2024-10-13 02:21:58 +0300

    latest commit before installing the Nexus generator

---
+++ dist/src/index.js
@@ -0,0 +1,40 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+// Type definitions (GraphQL schema)
+const typeDefs = `#graphql
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+// Sample data
+const books = [
+    {
+        title: "The Awakening",
+        author: "Kate Chopin",
+    },
+    {
+        title: "City of Glass",
+        author: "Paul Auster",
+    },
+];
+// Resolvers (functions that fetch the data for each query type)
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+};
+// Create Apollo Server instance
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+// Start the server
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+console.log(`🚀 Server ready at ${url}`);
